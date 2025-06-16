d69bfab2b1972a5e8261a2eba3678b6b19084e41
Артем Рябцев <artem@riabtsev.ru>
2025-06-16 13:14:50 +0300

    Добавить авторизацию

---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
+{
+    use TargetPathTrait;
+
+    public const LOGIN_ROUTE = 'app_login';
+
+    public function __construct(private readonly RouterInterface $router) {}
+
+    public function supports(Request $request): bool
+    {
+        return $request->attributes->get('_route') === self::LOGIN_ROUTE
+            && $request->isMethod('POST');
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $username = $request->request->get('username', '');
+        $password = $request->request->get('password', '');
+        $csrfToken = $request->request->get('_csrf_token');
+
+        return new Passport(
+            new UserBadge($username),
+            new PasswordCredentials($password),
+            [new CsrfTokenBadge('authenticate', $csrfToken)]
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): Response
+    {
+        $targetPath = $this->getTargetPath($request->getSession(), $firewallName);
+
+        return new RedirectResponse($targetPath ?: $this->router->generate('app_order_index'));
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    {
+        return new RedirectResponse($this->router->generate(self::LOGIN_ROUTE));
+    }
+
+    protected function getLoginUrl(Request $request): string
+    {
+        return $this->router->generate(self::LOGIN_ROUTE);
+    }
+}
