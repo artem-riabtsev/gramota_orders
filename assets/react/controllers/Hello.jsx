5eae22fb24574b732d5775fb927a43cc8eeec382
Артем Рябцев <artem@riabtsev.ru>
2026-03-28 14:50:21 +0300

    Редизайн навбара

--- assets/react/controllers/Hello.jsx
+++ assets/react/controllers/Hello.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Hello({ name }) {
+export default function Ping({ name }) {
     return (
         <div className="alert alert-info shadow-sm d-flex align-items-center" role="alert">
             <i className="bi bi-rocket-takeoff-fill me-3 fs-3"></i>
